49ac223e8019746b142346ea5a056d8429d0baee
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:39:23 +0545

    finishing up

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:provider_tutorial/providers/counter_provider.dart';
+import 'package:provider_tutorial/providers/shopping_cart_provider.dart';
 import 'package:provider_tutorial/screens/home_screen.dart';
+import 'package:provider_tutorial/screens/second_screen.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => Counter()),
+        ChangeNotifierProvider(create: (_) => ShoppingCart()),
       ],
       child: MyApp(),
     ),
