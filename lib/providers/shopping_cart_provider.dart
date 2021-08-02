f47b6207ca1a0ab0294bdd6bb5b7ca4b63f803ae
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:34:44 +0545

    Added shopping cart provider

--- lib/providers/shopping_cart_provider.dart
+++ lib/providers/shopping_cart_provider.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCart with ChangeNotifier {}
+class ShoppingCart with ChangeNotifier {
+  List<String> _shoppingCart = ['Apple', 'Orange', 'banana'];
+
+  int get count => _shoppingCart.length;
+  List<String> get cart => _shoppingCart;
+
+  void addItem(String item) {
+    _shoppingCart.add(item);
+    notifyListeners();
+  }
+}
