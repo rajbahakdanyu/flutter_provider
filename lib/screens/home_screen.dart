ce3a5fe75cfca1c744662eb62b83a130c0faf0ec
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:30:02 +0545

    Added Count widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,6 +20,7 @@ class MyHomePage extends StatelessWidget {
             Text(
               'You have pushed the button this many times: ${context.watch<Counter>().count}',
             ),
+            Count(),
           ],
         ),
       ),
@@ -52,6 +53,19 @@ class MyHomePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Count extends StatelessWidget {
+  const Count({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '${context.watch<Counter>().count}',
+      key: Key('counterState'),
+      style: Theme.of(context).textTheme.headline4,
     );
   }
 }
