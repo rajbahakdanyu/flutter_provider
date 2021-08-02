d52cfdad72a1073245507ac310b8ff301983a43a
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:36:50 +0545

    Added UI to second screen

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:provider_tutorial/providers/shopping_cart_provider.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider Example'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('${context.watch<ShoppingCart>().count}'),
+            Text('${context.watch<ShoppingCart>().cart}')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: Key('addItem_floatingActionButton'),
+        onPressed: () => context.read<ShoppingCart>().addItem('Bread'),
+        tooltip: 'Add Item',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
