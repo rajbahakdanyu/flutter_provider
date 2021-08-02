7ab6315f058eeaf2aca5a2fa0cc9c5741cfe7a08
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:25:56 +0545

    Added provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'screens/home_screen.dart';
+import 'package:provider_tutorial/providers/counter_provider.dart';
+import 'package:provider_tutorial/screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
