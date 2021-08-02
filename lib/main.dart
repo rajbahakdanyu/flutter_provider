008178e1f95a5cfb159aa251cd5711d556cdcd72
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-08-02 21:32:41 +0545

    Created second screen andd provider

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => MyHomePage(),
+        'second/': (context) => SecondPage(),
       },
     );
   }
